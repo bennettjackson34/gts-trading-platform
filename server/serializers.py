f9d6cffec75613753aea78523caa2113121317d6
bennettjackson34 <60453282+bennettjackson34@users.noreply.github.com>
2024-06-13 18:51:07 -0500

    deleting django files

--- server/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from .models import Trades
-class TradeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Trades
-        fields = ['id', 'asset', 'direction', 'entry_price', 'exit_price', 'risk_price', 'target_price']
